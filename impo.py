f056699aac5cf071572df4ba3ec4f62baacd3d87
Jaromrax <jaromrax@gmail.com>
2017-08-02 16:55:00 +0200

    impo.py - RUNs with mymod (good for pureborg)

--- impo.py
+++ impo.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 import mymod  #.py appended automatically
-from mymod import logge0,args,command_parser_init,command_parser_step
 
+mymod.argparse_ini()
+mymod.argparse_fin()
+mymod.logging_ini()
+mymod.logging_fin()
+from mymod import logger,logger_head
+
+
+import os
+import subprocess
+import time
+
+
+#from mymod import logge0,args,command_parser_init,command_parser_step
+from mymod import command_parser_init,command_parser_step
+
+#################################
+#
+#  MAIN
+#
+#################################
 if __name__ == "__main__":
-    logge0.info('====== START argument=%s ====',args.book)  # start LOG file
+    #logge0.info('====== START argument=%s ====',args.book)  # start LOG file
     #- init command parser
     poller,receiver,collecter_data=command_parser_init()
     x=0
